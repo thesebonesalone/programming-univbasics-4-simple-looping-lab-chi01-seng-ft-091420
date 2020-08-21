3985aae156c989dccb006071876c5bdee8be1068
Willem A Duiker <duikhead2@gmail.com>
2020-08-21 16:08:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,16 @@ def loop_message_five_times(message)
     counter += 1
   end
 end
+
+def loop_message_n_times(message,n)
+  counter = 0
+  while counter < n do
+    puts message
+    counter += 1
+  end
+end
+
+def output_array(message,n)
+array = []
+array.push(message,n)
+puts array
